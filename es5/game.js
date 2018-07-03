c4f2ff9177bf4fec7dc9b28c212fd63652244a8c
Chris Zelenka <Zelenka.chris@gmail.com>
2018-07-03 03:19:49 -0700

    Add controls and gravity.

--- es5/game.js
+++ es5/game.js
@@ -4,19 +4,37 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+var playerSpeed = 10;
+
 var player;
 
-function main() {
-  player = new Player(10, 10, 50, 50);
+var Key = {
+  pressed: {},
 
-  loop();
-}
+  UP: 38,
+  DOWN: 40,
+  LEFT: 37,
+  RIGHT: 39,
 
-function loop() {
-  control();
-  redraw();
-  requestAnimationFrame(loop);
-}
+  isDown: function isDown(keyCode) {
+    return this.pressed[keyCode];
+  },
+
+  onKeydown: function onKeydown(event) {
+    this.pressed[event.keyCode] = true;
+  },
+
+  onKeyup: function onKeyup(event) {
+    delete this.pressed[event.keyCode];
+  }
+};
+
+window.addEventListener('keyup', function (event) {
+  Key.onKeyup(event);
+}, false);
+window.addEventListener('keydown', function (event) {
+  Key.onKeydown(event);
+}, false);
 
 var Player = function () {
   function Player(x, y, h, w) {
@@ -25,7 +43,9 @@ var Player = function () {
     this.position = [x, y];
     this.size = [h, w];
     this.velocity = [0, 0];
+    this.acceleration = [0, 1];
     this.fillstyle = 'green';
+    this.isJump = false;
   }
 
   _createClass(Player, [{
@@ -37,19 +57,68 @@ var Player = function () {
   }, {
     key: 'move',
     value: function move(x, y) {
-      this.position[0] += x;
-      this.position[1] += y;
+      this.velocity[0] = x;
+      this.velocity[1] += y + this.acceleration[1];
+      this.position[0] += this.velocity[0];
+      this.position[1] += this.velocity[1];
     }
   }]);
 
   return Player;
 }();
 
+function loop() {
+  control();
+  redraw();
+  requestAnimationFrame(loop);
+}
+
 function redraw() {
   ctx.clearRect(0, 0, 1280, 720);
   player.draw();
 }
 
+function playerMove() {
+  var x = 0;
+  var y = 0;
+  if (Key.isDown(Key.UP) && !player.isJump) {
+    y += -playerSpeed * 3;
+    player.isJump = true;
+  }
+  if (Key.isDown(Key.LEFT)) {
+    x += -playerSpeed;
+  }
+  if (Key.isDown(Key.RIGHT)) {
+    x += playerSpeed;
+  }
+
+  player.move(x, y);
+}
+
+function checkValidMove() {
+  if (player.position[0] < 0) {
+    player.position[0] = 0;
+  }
+  if (player.position[0] + player.size[0] > 1280) {
+    player.position[0] = 1280 - player.size[0];
+  }
+
+  if (player.position[1] < 0) {
+    player.position[1] = 0;
+  }
+  if (player.position[1] + player.size[1] > 720) {
+    player.position[1] = 720 - player.size[1];
+    player.velocity[1] = 0;
+    player.isJump = false;
+  }
+}
+
 function control() {
-  player.move(1, 1);
+  playerMove();
+  checkValidMove();
+}
+
+function main() {
+  player = new Player(10, 10, 50, 50);
+  loop();
 }
--- es6/game.js
+++ es6/game.js
@@ -1,24 +1,39 @@
+const playerSpeed = 10;
+
 var player;
 
+var Key = {
+    pressed: {},
 
-function main() {
-  player = new Player(10,10,50,50);
+    UP: 38,
+    DOWN: 40,
+    LEFT: 37,
+    RIGHT: 39,
+  
+    isDown: function(keyCode) {
+        return this.pressed[keyCode];
+    },
+  
+    onKeydown: function(event) {
+        this.pressed[event.keyCode] = true;
+    },
+  
+    onKeyup: function(event) {
+        delete this.pressed[event.keyCode];
+    }
+};
 
-  loop();
-}
-
-function loop() {
-  control();
-  redraw();
-  requestAnimationFrame(loop);
-}
+window.addEventListener('keyup', function(event) { Key.onKeyup(event); }, false);
+window.addEventListener('keydown', function(event) { Key.onKeydown(event); }, false);
 
 class Player {
   constructor(x, y, h, w) {
     this.position = [x,y];
     this.size = [h,w];
     this.velocity = [0,0];
+    this.acceleration = [0,1];
     this.fillstyle = 'green';
+    this.isJump = false;
   }
   
   draw() {
@@ -30,17 +45,68 @@ class Player {
   }
   
   move(x, y) {
-    this.position[0] += x;
-    this.position[1] += y;
-  }  
+    this.velocity[0] = x;
+    this.velocity[1] += y + this.acceleration[1];
+    this.position[0] += this.velocity[0];
+    this.position[1] += this.velocity[1];
+  }
 }
 
+function loop() {
+  control();
+  redraw();
+  requestAnimationFrame(loop);
+}
 
 function redraw() {
   ctx.clearRect(0,0,1280,720);
   player.draw();
 }
 
-function control() {
-  player.move(1,1);
+function playerMove() {
+  let x = 0;
+  let y = 0;
+  if ( Key.isDown(Key.UP) && !player.isJump) {
+    y += -playerSpeed * 3;
+    player.isJump = true;
+  }
+  if ( Key.isDown(Key.LEFT) ) {
+    x += -playerSpeed;
+  }
+  if ( Key.isDown(Key.RIGHT) ) {
+    x += playerSpeed;
+  }
+  
+  player.move(x,y);
 }
+
+function checkValidMove() {
+  if ( player.position[0] < 0 ) {
+    player.position[0] = 0;
+  }
+  if ( player.position[0] + player.size[0] > 1280 ) {
+    player.position[0] = 1280 - player.size[0];
+  }
+  
+  if ( player.position[1] < 0 ) {
+    player.position[1] = 0;
+  }
+  if ( player.position[1] + player.size[1] > 720 ) {
+    player.position[1] = 720 - player.size[1];
+    player.velocity[1] = 0;
+    player.isJump = false;
+  }
+}
+
+function control() {
+  playerMove();
+  checkValidMove();
+}
+
+
+
+function main() {
+  player = new Player(10,10,50,50);
+  loop();
+}
+

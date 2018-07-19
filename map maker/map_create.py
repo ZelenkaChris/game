f334ef58b0af55e7bca0afbb97de5ee0040532cb
Chris Zelenka <Zelenka.chris@gmail.com>
2018-07-18 19:58:30 -0700

    start rectangle search algorithm

--- map maker/map_create.py
+++ map maker/map_create.py
@@ -2,7 +2,11 @@ from Tkinter import *
 
 block_size = 40
 
-level = [[False for y in range(32)] for x in range(18)]   #[y][x] // [row][col]
+level = [[0 for y in range(32)] for x in range(18)]   #[y][x] // [row][col]
+
+testLevel = [[0 for y in range(4)] for x in range(4)]
+testLevel[0] = [1, 1, 1, 1]
+testLevel[1] = [1, 1, 1, 0]
 
 app = Tk()
 app.title("Map Maker")
@@ -13,7 +17,7 @@ def add_menus(a):
     menubar = Menu(a)
     filemenu = Menu(menubar, tearoff=0)
     filemenu.add_command(label="New", command=None)
-    filemenu.add_command(label="Open", command=None)
+    filemenu.add_command(label="Open", command=testSave)
     filemenu.add_command(label="Save as...", command=save)
     filemenu.add_separator()
     filemenu.add_command(label="Exit", command=app.quit)
@@ -24,18 +28,27 @@ def add_menus(a):
 def click(e):
     x = e.x/block_size
     y = e.y/block_size
-    if not level[y][x]:
-        level[y][x] = True
+    if level[y][x] != 1:
+        level[y][x] = 1
         canvas.create_rectangle(x * block_size, y * block_size, x * block_size + block_size,  y * block_size + block_size, fill="black")
 
 def save():
     for i in range(len(level)):
-        for j in range(len(level[i])):
-            if level[i][j]:
-                print "1",
+        print level[i]
+
+
+def testSave():
+    row = [0 for i in range(len(testLevel[0]))]
+
+    for i in range(len(testLevel)):
+        for j in range(len(testLevel[i])):
+            val = testLevel[i][j]
+            if (val != 0):
+                row[j] += 1
             else:
-                print "0",
-        print
+                row[j] = 0
+        print row
+        
         
 
 canvas = Canvas(app)

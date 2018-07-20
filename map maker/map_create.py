96da1e085a0db8a6c1ccd9798a013d1860dc7da5
Chris Zelenka <Zelenka.chris@gmail.com>
2018-07-19 20:23:28 -0700

    .

--- map maker/map_create.py
+++ map maker/map_create.py
@@ -5,8 +5,12 @@ block_size = 40
 level = [[0 for y in range(32)] for x in range(18)]   #[y][x] // [row][col]
 
 testLevel = [[0 for y in range(4)] for x in range(4)]
-testLevel[0] = [1, 1, 1, 1]
-testLevel[1] = [1, 1, 1, 0]
+testCond = [[0 for y in range(4)] for x in range(4)]
+
+testLevel[0] = [1, 1, 0, 1]
+testLevel[1] = [1, 1, 0, 1]
+testLevel[2] = [1, 1, 1, 0]
+testLevel[3] = [1, 1, 0, 1]
 
 app = Tk()
 app.title("Map Maker")
@@ -38,8 +42,11 @@ def save():
 
 
 def testSave():
+    resArr = [[0 for y in range(len(testLevel))] for x in range(len(testLevel[0]))]
     row = [0 for i in range(len(testLevel[0]))]
-
+    retArr = [[0 for y in range(len(testLevel))] for x in range(len(testLevel[0]))]
+    
+    '''
     for i in range(len(testLevel)):
         for j in range(len(testLevel[i])):
             val = testLevel[i][j]
@@ -47,7 +54,46 @@ def testSave():
                 row[j] += 1
             else:
                 row[j] = 0
-        print row
+        #print row
+        count = 0
+        minHeight = 999
+        for x in range(len(row)):
+            if (row[x] >= 1):
+                count += 1
+                minHeight = minHeight if minHeight < row[x] else row[x]
+            else:
+                count = 0
+                minHeight = 999
+
+            if (count > 0):
+                testCond[i][x - count + 1] = [count, minHeight]
+        print testCond[i]
+    '''
+
+    for i in reversed(range(len(testLevel))):
+        for j in reversed(range(len(testLevel[i]))):
+            val = testLevel[i][j]
+            if (val != 0):
+                row[j] += 1
+            else:
+                row[j] = 0
+        resArr[i] = list(row)
+    
+    for i in range(len(resArr)):
+        count = 0
+        minHeight = 999
+        for j in reversed(range(len(resArr[i]))):
+            val = resArr[i][j]
+            if (val >= 1):
+                count += 1
+                minHeight = minHeight if minHeight < val else val
+            else :
+                count = 0
+                minHeight = 999
+            retArr[i][j] = [count, minHeight if minHeight != 999 else 0]
+            print retArr[i][j],
+        print
+            
         
         
 
@@ -55,6 +101,8 @@ canvas = Canvas(app)
 canvas.bind("<Button-1>", click)
 canvas.pack(fill="both", expand=True)
 
-add_menus(app)
+#add_menus(app)
 
-app.mainloop()
+#app.mainloop()
+
+testSave()
